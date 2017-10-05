dd5a2857fb7b201a61907f2fbf4927e999df8ecd
JoeZuhusky <jzuhusky@gmail.com>
2017-10-05 13:47:23 +0000

    Created Temp /promotions page; added info doc string

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, url_for
+import os
 
 flask_app = Flask(__name__)
 
@@ -8,15 +9,30 @@ PORT = os.getenv('PORT', '5001') # NOTICE PORT 5001 !!!!
 HOSTNAME = os.getenv('HOSTNAME', '127.0.0.1')
 REDIS_PORT = os.getenv('REDIS_PORT', '6379')
 
-# Flask Routes
+"""
+Promotion  Service
 
-@app.route('/')
+Available Resources:
+------
+GET /promotions - Returns a list all of the Active Promotions
+GET /promotions/{id} - Returns the Pet with a given promot-id number
+                       Almost Like a "PromoCode"
+POST /promotion - creates a new Promotion in the database
+PUT /promotion/{id} - updates a Promotion record in the database
+DELETE /pets/{id} - deletes a Promotion record in the database
+"""
+
+# Flask Main App Route
+# See routes.py for Business Logic / Path code
+
+@flask_app.route('/promotions')
 def index():
     '''Returns a message about the service'''
-    return jsonify(name='You\'ve hit the Promo Service',
-                   version='1.0',
-                   url=url_for('hits', _external=True)
-                  ), 200
+    payload = {}
+    payload['info'] = 'Promotion Service Main Page!'
+    payload['data'] = {"Urls": "Put Something Here"}
+    payload['version'] = '1.0'
+    return jsonify(payload), 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
+    flask_app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
