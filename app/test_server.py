58ed2c137b0247ddf90338534e63d5c2c3d46093
Diqing Zhu <angelajubejudy@yahoo.com.hk>
2017-10-11 16:32:21 -0400

    Add a unit test for function 'get_promotion'.
    Test by giving an ID of existing promo and expect to get the right name of that promo.

--- app/test_server.py
+++ app/test_server.py
@@ -9,6 +9,11 @@ class TestCaseZero(unittest.TestCase):
     self.promotion = None
     
   '''List tests for each system functions'''
+  def test_get_promotion(self, promo_id):
+    resp = self.flask_app.get_promotion('/promotions/1')
+    self.assertEqual(resp.status_code, 200)
+    data = json.loads(resp.data)
+    self.assertEqual(data['name'], 'promo_1')
     
 if __name__ == '__main__': 
   unittest.main()
