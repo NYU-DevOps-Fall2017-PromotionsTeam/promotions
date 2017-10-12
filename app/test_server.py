841798452699b2bfaf5cb5ab9dc189b27ef8dde4
JoeZuhusky <jzuhusky@gmail.com>
2017-10-12 14:38:49 +0000

    Added Skeleton Code for tests; added *.swp to gitignore

---
+++ app/test_server.py
@@ -0,0 +1,27 @@
+import unittest
+
+class TestServer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_index(self):
+        pass
+
+    def test_get_promotion(self):
+        pass
+
+    def test_create_promotion(self):
+        pass
+
+    def test_update_promotion(self):
+        pass
+
+    def test_delete_promotion(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
