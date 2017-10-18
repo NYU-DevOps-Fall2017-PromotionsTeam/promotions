5046bb9f4f9330ce0190251e753558090dfdc97b
JoeZuhusky <jzuhusky@gmail.com>
2017-10-18 17:52:11 +0000

    More Tests

--- app/test_server.py
+++ app/test_server.py
@@ -14,9 +14,10 @@ class TestServer(unittest.TestCase):
 
     def setUp(self):
         self.app = flask_app.test_client()
+        Promotion.data = []
 
     def tearDown(self):
-        pass
+        Promotion.data = [] # Final Clean up
 
     def test_list_promotions(self):
         '''Test list all promotions'''
@@ -27,8 +28,8 @@ class TestServer(unittest.TestCase):
             self.app.post('/promotions', data=json.dumps({}), content_type='application/json')
         resp = self.app.get('/promotions')
         data = json.loads(resp.data.decode('utf-8'))
+        self.assertIsInstance(data, list)
         self.assertEqual(len(data), 5)
-        # Clean up 
         Promotion.data = []
 
     def test_get_promotion(self):
@@ -39,7 +40,10 @@ class TestServer(unittest.TestCase):
         resp = self.app.get('/promotions/1')
         self.assertEqual(resp.status_code, 200)
         data = json.loads(resp.data.decode('utf-8'))
+        self.assertIsInstance(data, dict)
         self.assertEqual(data['name'], 'default')
+        self.assertEqual(data['value'], 0.0)
+        self.assertEqual(data['id'], 1)
         promo.delete()
 
     def test_bad_get_by_id(self):
@@ -103,6 +107,26 @@ class TestServer(unittest.TestCase):
         data = json.dumps(params)
         resp = self.app.post('/promotions', data=data, content_type='application/json')
         self.assertEqual(resp.status_code, 400) 
+        params = {
+            'name': 'test',
+            'value': 10.0,
+            'detail': 'test',
+            'promo_type': 'A',
+            'start_date': '2017-01-01 11:11:11', # notice bad time stamps
+            'end_date': '2018-02-02 11:11:11'
+        }
+        resp = self.app.post('/promotions', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, 400) 
+        params = {
+            'name': 'test',
+            'value': "SOME STRING",
+            'detail': 'test',
+            'promo_type': '%',
+            'start_date': '2017-01-01 11:11:11', # notice bad time stamps
+            'end_date': '2018-02-02 11:11:11'
+        }
+        resp = self.app.post('/promotions', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, 400) 
 
     def test_update_promotion(self):
         '''Do a put to update some data'''
@@ -111,14 +135,18 @@ class TestServer(unittest.TestCase):
             'value': 10,
             'detail': 'test'
         }
-        response = self.app.post('/promotions', data=json.dumps(params), content_type='application/json')
-        promo = Promotion.all()[0]
+        promo = Promotion()
+        promo.deserialize(params) 
+        promo.save()
         self.assertEqual(promo.name, 'OLDNAME')
+        self.assertEqual(promo.value, 10)
+        self.assertEqual(promo.detail, 'test')
         _id = promo.id
-        data = {'name': 'NEWNAME'}
+        data = {'name': 'NEWNAME', 'value': 11, 'detail': 'TEST'}
         response = self.app.put('/promotions/'+str(_id), data=json.dumps(data), content_type='application/json')
-        promo = Promotion.all()[0]
         self.assertEqual(promo.name, 'NEWNAME')
+        self.assertEqual(promo.value, 11)
+        self.assertEqual(promo.detail, 'TEST')
         promo.delete()
 
     def test_bad_update(self):
@@ -138,17 +166,17 @@ class TestServer(unittest.TestCase):
         resp = self.app.delete('/promotions/%d' % 100)
         self.assertEqual(Promotion.all(), [])
         self.assertEqual(resp.status_code, 200)
-        self.app.post('/promotions', data=json.dumps({}), content_type='application/json')
+        promo = Promotion()
+        promo.save()
         self.assertEqual(len(Promotion.all()), 1)
-        promo = Promotion.all()[0]
         _id = promo.id
         resp = self.app.delete('/promotions/%d' % _id)
         self.assertEqual(Promotion.all(), [])
         self.assertEqual(resp.status_code, 200)
-        
 
     def test_write_to_file(self):
         '''Test Write to file'''
+        Promotion.data = []
         promo = Promotion()
         promo.save()
         resp = self.app.put('/promotions/write-to-file')
@@ -160,6 +188,7 @@ class TestServer(unittest.TestCase):
         promo.delete()
 
     def test_check_content_type(self):
+        '''Basic Check to ensure util func is working'''
         resp = self.app.post('/promotions', data=json.dumps({}), content_type='application/xml')
         self.assertEqual(resp.status_code, 415)
         resp = self.app.post('/promotions', data=json.dumps({}), content_type='application/json')
