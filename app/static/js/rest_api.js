5e0c9b5e1d8c94a934832599b59a155b38923d75
Da Ying <dy877@nyu.edu>
2017-12-11 01:14:38 -0500

    finish ui

--- app/static/js/rest_api.js
+++ app/static/js/rest_api.js
@@ -6,21 +6,24 @@ $(function () {
 
     // Updates the form with data from the response
     function update_form_data(res) {
-        $("#pet_id").val(res.id);
-        $("#pet_name").val(res.name);
-        $("#pet_category").val(res.category);
-        if (res.available == true) {
-            $("#pet_available").val("true");
-        } else {
-            $("#pet_available").val("false");
-        }
+        $("#promo_id").val(res.id);
+        $("#promo_name").val(res.name);
+        $("#promo_type").val(res.promo_type);
+        $("#promo_value").val(res.value);
+        $("#promo_start_date").val(res.start_date);
+        $("#promo_end_date").val(res.end_date);
+        $("#promo_detail").val(res.detail);
     }
 
     /// Clears all form fields
     function clear_form_data() {
-        $("#pet_name").val("");
-        $("#pet_category").val("");
-        $("#pet_available").val("");
+        $("#promo_name").val("");
+        $("#promo_type").val("");
+        $("#promo_value").val("");
+        $("#promo_start_date").val("");
+        $("#promo_end_date").val("");
+        $("#promo_detail").val("");
+        $("#promo_available_date").val("");
     }
 
     // Updates the flash message area
@@ -30,96 +33,120 @@ $(function () {
     }
 
     // ****************************************
-    // Create a Pet
+    // Create a Promotion
     // ****************************************
 
     $("#create-btn").click(function () {
 
-        var name = $("#pet_name").val();
-        var category = $("#pet_category").val();
-        var available = $("#pet_available").val() == "true";
+        var name = $("#promo_name").val();
+        var type = $("#promo_type").val();
+        var value = Number($("#promo_value").val());
+        var start_date = $("#promo_start_date").val();
+        var end_date = $("#promo_end_date").val();
+        var detail = $("#promo_detail").val();
 
-        var data = {
-            "name": name,
-            "category": category,
-            "available": available
-        };
+        var data = {};
+
+        if (name)
+            data.name = name;
+        if (type)
+            data.promo_type = type;
+        if (value)
+            data.value = value;
+        if (start_date)
+            data.start_date = start_date
+        if (end_date)
+            data.end_date = end_date
+        if (detail)
+            data.detail = detail
 
         var ajax = $.ajax({
             type: "POST",
-            url: "/pets",
-            contentType:"application/json",
+            url: "/promotions",
+            contentType: "application/json",
             data: JSON.stringify(data),
         });
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             update_form_data(res)
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
     });
 
 
     // ****************************************
-    // Update a Pet
+    // Update a Promotion
     // ****************************************
 
     $("#update-btn").click(function () {
 
-        var pet_id = $("#pet_id").val();
-        var name = $("#pet_name").val();
-        var category = $("#pet_category").val();
-        var available = $("#pet_available").val() == "true";
+        var id = $("#promo_id").val();
+        var name = $("#promo_name").val();
+        var type = $("#promo_type").val();
+        var value = Number($("#promo_value").val());
+        var start_date = $("#promo_start_date").val();
+        var end_date = $("#promo_end_date").val();
+        var detail = $("#promo_detail").val();
 
-        var data = {
-            "name": name,
-            "category": category,
-            "available": available
-        };
+        data = {};
+
+        if (name)
+            data.name = name;
+        if (type)
+            data.promo_type = type;
+        if (value)
+            data.value = value;
+        if (start_date)
+            data.start_date = start_date
+        if (end_date)
+            data.end_date = end_date
+        if (detail)
+            data.detail = detail
 
         var ajax = $.ajax({
-                type: "PUT",
-                url: "/pets/" + pet_id,
-                contentType:"application/json",
-                data: JSON.stringify(data)
-            })
+            type: "PUT",
+            url: "/promotions/" + id,
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             update_form_data(res)
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
 
     });
 
     // ****************************************
-    // Retrieve a Pet
+    // Retrieve a Promotion
     // ****************************************
 
     $("#retrieve-btn").click(function () {
 
-        var pet_id = $("#pet_id").val();
+        var promo_id = $("#promo_id").val();
 
         var ajax = $.ajax({
             type: "GET",
-            url: "/pets/" + pet_id,
-            contentType:"application/json",
+            url: "/promotions/" + promo_id,
+            contentType: "application/json",
             data: ''
         })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             //alert(res.toSource())
             update_form_data(res)
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             clear_form_data()
             flash_message(res.responseJSON.message)
         });
@@ -127,26 +154,26 @@ $(function () {
     });
 
     // ****************************************
-    // Delete a Pet
+    // Delete a Promotion
     // ****************************************
 
     $("#delete-btn").click(function () {
 
-        var pet_id = $("#pet_id").val();
+        var promo_id = $("#promo_id").val();
 
         var ajax = $.ajax({
             type: "DELETE",
-            url: "/pets/" + pet_id,
-            contentType:"application/json",
+            url: "/promotions/" + promo_id,
+            contentType: "application/json",
             data: '',
         })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             clear_form_data()
-            flash_message("Pet with ID [" + res.id + "] has been Deleted!")
+            flash_message("Promotion with ID [" + promo_id + "] has been Deleted!")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message("Server error!")
         });
     });
@@ -156,60 +183,69 @@ $(function () {
     // ****************************************
 
     $("#clear-btn").click(function () {
-        $("#pet_id").val("");
+        console.log("haha")
+        $("#promo_id").val("");
         clear_form_data()
     });
 
     // ****************************************
-    // Search for a Pet
+    // Search for a Promotion
     // ****************************************
 
     $("#search-btn").click(function () {
 
-        var name = $("#pet_name").val();
-        var category = $("#pet_category").val();
-        var available = $("#pet_available").val() == "true";
+        var type = $("#promo_type").val();
+        var available_date = $("#promo_available_date").val();
 
         var queryString = ""
 
-        if (name) {
-            queryString += 'name=' + name
+        if (type) {
+            queryString += 'promo_type=' + type
         }
-        if (category) {
+        if (available_date) {
             if (queryString.length > 0) {
-                queryString += '&category=' + category
+                queryString += '&available_on=' + available_date
             } else {
-                queryString += 'category=' + category
-            }
-        }
-        if (available) {
-            if (queryString.length > 0) {
-                queryString += '&available=' + available
-            } else {
-                queryString += 'available=' + available
+                queryString += 'available_on=' + available_date
             }
         }
 
-        var ajax = $.ajax({
-            type: "GET",
-            url: "/pets?" + queryString,
-            contentType:"application/json",
-            data: ''
-        })
+        if (queryString){
+            var ajax = $.ajax({
+                type: "GET",
+                url: "/promotions?" + queryString,
+                contentType: "application/json",
+                data: ''
+            })
+        }
+        else {
+            var ajax = $.ajax({
+                type: "GET",
+                url: "/promotions",
+                contentType: "application/json",
+                data: ''
+            })
+        }
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             //alert(res.toSource())
             $("#search_results").empty();
             $("#search_results").append('<table class="table-striped">');
             var header = '<tr>'
+
             header += '<th style="width:10%">ID</th>'
-            header += '<th style="width:40%">Name</th>'
-            header += '<th style="width:40%">Category</th>'
-            header += '<th style="width:10%">Available</th></tr>'
+            header += '<th style="width:10%">Name</th>'
+            header += '<th style="width:10%">Type</th>'
+            header += '<th style="width:10%">Value</th>'
+            header += '<th style="width:30%">Start Date</th>'
+            header += '<th style="width:30%">End Date</th>'
+            header += '<th style="width:30%">Detail</th>'
+
             $("#search_results").append(header);
-            for(var i = 0; i < res.length; i++) {
-                pet = res[i];
-                var row = "<tr><td>"+pet.id+"</td><td>"+pet.name+"</td><td>"+pet.category+"</td><td>"+pet.available+"</td></tr>";
+            for (var i = 0; i < res.length; i++) {
+                promo = res[i];
+                var row = "<tr><td>" + promo.id + "</td><td>" + promo.name + "</td><td>" + promo.promo_type + "</td><td>" + promo.value + "</td>";
+                row += "<td>" + promo.start_date + "</td><td>" + promo.end_date + "</td><td>" + promo.detail + "</td></tr>";
                 $("#search_results").append(row);
             }
 
@@ -218,7 +254,7 @@ $(function () {
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
 
