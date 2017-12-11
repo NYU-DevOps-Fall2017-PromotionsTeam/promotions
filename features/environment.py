3e7cf3e7638ff4a55385dcd95786671804e88546
Da Ying <dy877@nyu.edu>
2017-12-11 17:57:14 -0500

    try fix selenium

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,20 @@
+import os
 from behave import *
 from app import server
+from selenium import webdriver
+import urllib.request
+
+BASE_URL = os.getenv('BASE_URL', 'http://localhost:5001')
 
 def before_all(context):
     context.app = server.flask_app.test_client()
     server.init_db()
+
+    context.driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
+    context.driver.implicitly_wait(10) # seconds
+    context.driver.set_window_size(1120, 550)
+    context.base_url = BASE_URL
+
+
+    
+    urllib.request.urlopen("http://localhost:5001").read()
